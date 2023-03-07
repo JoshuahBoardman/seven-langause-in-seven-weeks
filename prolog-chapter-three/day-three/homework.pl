b2c2cf2706e67c76fcca97041f2958c3cdf644ad
Joshuah Boardman <joshuahboardman@gmail.com>
2023-03-06 23:25:30 -0600

    Update: Started working on a modular sukoku solver in prlog

--- prolog-chapter-three/day-three/homework.pl
+++ prolog-chapter-three/day-three/homework.pl
@@ -1,22 +1,46 @@
-valid_bored([]).
-valid_bored([Head|Tail]) :-
+valid_board([]).
+valid_board([Head|Tail]) :-
   fd_all_different(Head), 
-  valid_bored(Tail).
+  valid_board(Tail).
 
 format_sudoku([], _).
 format_sudoku(List, N) :-
     append(Chunk, Rest, List),
     length(Chunk, N),
     write(Chunk), nl,
-    format_list(Rest, N).
+    format_sudoku(Rest, N).
 
-sudoku(Bored, Solution) :-
-  Solution = Bored, 
-  Bored = [ A1, B1, C1, D1, 
+valid_board_size([], 0).
+valid_board_size(Board, Size) :-
+    length(Board, List_size),
+    Size is floor(sqrt(List_size)),
+    Size ^ 2 =:= List_size.
+
+
+sudoku_rows([], _, []).
+sudoku_rows(Board, Size, [H|T]) :- 
+   length(H, Size),
+   append(H, Rest, Board),
+   sudoku_rows(Rest, Size, T). 
+
+
+sudoku(Board, Solution) :-
+    valid_board_size(Board, Board_Size),
+    fd_domain(Board, 1, Board_Size),
+    Solution = Board,
+    sudoku_rows(Board, Board_Sized, Rows),
+    valid_board(Rows).
+
+
+
+
+/*sudoku(Board, Solution) :-
+  Solution = Board, 
+  Board = [ A1, B1, C1, D1, 
             A2, B2, C2, D2, 
             A3, B3, C3, D3, 
             A4, B4, C4, D4], 
-  fd_domain(Bored, 1, 4), 
+  fd_domain(Board, 1, 4), 
 
   Row1 = [A1, B1, C1, D1], 
   Row2 = [A2, B2, C2, D2], 
@@ -33,20 +57,20 @@ sudoku(Bored, Solution) :-
   Square3 = [A3, B3, A4, B4], 
   Square4 = [C3, D3, C4, D4], 
 
-  valid_bored([ Row1, Row2, Row3, Row4, 
+  valid_board([ Row1, Row2, Row3, Row4, 
                 Column1, Column2, Column3, Column4, 
                 Square1, Square2, Square3, Square4]),
-       format_sudoku(Solution, 4). 
+       format_sudoku(Solution, 4).*/ 
 
-/*sudoku(Bored, Solution) :-
-  Solution = Bored, 
-  Bored = [ A1, B1, C1, D1, E1, F1, 
+/*sudoku(Board, Solution) :-
+  Solution = Board, 
+  Board = [ A1, B1, C1, D1, E1, F1, 
             A2, B2, C2, D2, E2, F2, 
             A3, B3, C3, D3, E3, F3, 
             A4, B4, C4, D4, E4, F4,
             A5, B5, C5, D5, E5, F5,
             A6, B6, C6, D6, E6, F6], 
-     fd_domain(Bored, 1, 6), 
+     fd_domain(Board, 1, 6), 
 
   Row1 = [A1, B1, C1, D1, E1, F1], 
   Row2 = [A2, B2, C2, D2, E2, F2], 
@@ -69,14 +93,14 @@ sudoku(Bored, Solution) :-
   Square5 = [A5, B5, C5, A6, B6, C6],
   Square6 = [D5, E5, F5, D6, E6, F6],
 
-  valid_bored([ Row1, Row2, Row3, Row4, Row5, Row6, 
+  valid_board([ Row1, Row2, Row3, Row4, Row5, Row6, 
                 Column1, Column2, Column3, Column4, Column5, Column6, 
                 Square1, Square2, Square3, Square4, Square5, Square6]).
 
 
-sudoku(Bored, Solution) :-
-  Solution = Bored, 
-  Bored = [ A1, B1, C1, D1, E1, F1, G1, H1, I1, 
+sudoku(Board, Solution) :-
+  Solution = Board, 
+  Board = [ A1, B1, C1, D1, E1, F1, G1, H1, I1, 
             A2, B2, C2, D2, E2, F2, G2, H2, I2, 
             A3, B3, C3, D3, E3, F3, G3, H3, I3, 
             A4, B4, C4, D4, E4, F4, G4, H4, I4,
@@ -85,7 +109,7 @@ sudoku(Bored, Solution) :-
             A7, B7, C7, D7, E7, F7, G7, H7, I7,
             A8, B8, C8, D8, E8, F8, G8, H8, I8, 
             A9, B9, C9, D9, E9, F9, G9, H9, I9],
-     fd_domain(Bored, 1, 9), 
+     fd_domain(Board, 1, 9), 
 
   Row1 = [A1, B1, C1, D1, E1, F1, G1, H1, I1], 
   Row2 = [A2, B2, C2, D2, E2, F2, G2, H2, I2], 
@@ -117,6 +141,6 @@ sudoku(Bored, Solution) :-
   Square8 = [D7, E7, F7, D8, E8, F8, D9, E9, F9],
   Square9 = [G7, H7, I7, G8, H8, I8, G9, H9, I9],
 
-  valid_bored([ Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9, 
+  valid_board([ Row1, Row2, Row3, Row4, Row5, Row6, Row7, Row8, Row9, 
                 Column1, Column2, Column3, Column4, Column5, Column6, Column7, Column8, Column9, 
                 Square1, Square2, Square3, Square4, Square5, Square6, Square7, Square8, Square9]).*/
