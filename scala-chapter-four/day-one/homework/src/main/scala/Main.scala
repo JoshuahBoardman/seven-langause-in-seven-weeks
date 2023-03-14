59b7678561c892dc936302445503b0a6fb42998c
Joshuah Boardman <joshuahboardman@gmail.com>
2023-03-13 22:56:29 -0500

    Update: finished the day-one tictactoe homework

--- scala-chapter-four/day-one/homework/src/main/scala/Main.scala
+++ scala-chapter-four/day-one/homework/src/main/scala/Main.scala
@@ -1,54 +1,128 @@
 @main def board: Unit = {
   val stuff = new TicTacToe(
-    ("X", "X", "X"),
+    ("O", "O", "X"),
     ("X", "X", "O"),
-    ("O", "X", "_")) 
-  stuff.winByRow()
-  stuff.winByCollom()
-  stuff.winByDiagnal()
+    ("O", "X", "X")) 
   stuff.printBoard()
+  stuff.evaluateBoard()
 } 
 
 class TicTacToe(row1: Tuple3[String, String, String],
   row2: Tuple3[String, String, String],
   row3: Tuple3[String, String, String]) {
 
-  /*def validBoard() = {
-    val row1 = this.row1
-    val row2 = this.row2
-    val row3 = this.row3
+  def evaluateBoard(): Unit = {
+    val correctMoves = checkMoveNumber()
+    val winningMarking = checkForWin()
+    val fullBoard = checkForFullBoard()
+    //val correctInput = checkforincorrectinput()
+
+    /*if (!correctInput) {
+      println("All marks on the board must be an O, X, or blank")
+      return
+    }*/ 
+
+    if (!correctMoves) {
+      println("The order of moves played is invalid")
+      return
+    }
+
+    if (winningMarking != "") println(winningMarking + " is the winner")
+    if (winningMarking == "") println("There is no winner")
+    if (winningMarking == "" && fullBoard) println ("The game is a draw") 
+    if (winningMarking == "" && !fullBoard) println ("But the game will go on") 
+  } 
+
+  /*def checkforincorrectinput(): Boolean = {
+    var index = 0
+    var correctInput = true
+    row1.toList.foreach { element => 
+        if ((element != "O" || element != "X" || element != "") ||
+          (row2(index) != "O" || row2(index) != "X" || row2(index) != "") ||
+          (row3(index) != "O" || row3(index) != "X" || row3(index) != "")) {
+            correctInput = false
+          }
+          index += 1
+      }
+    println(correctInput)
+    return correctInput
   }*/
 
-  def winByRow( ): Boolean = {
-    val row1 = this.row1.toList
-    val row2 = this.row2.toList
-    val row3 = this.row3.toList
-     
-    val winByRow = row1.distinct.length == 1 ||
-      row2.distinct.length == 1 ||
-      row3.distinct.length == 1
-   
-    return winByRow
+  def checkMoveNumber(): Boolean = {
+    val boardValue = getBoardValues() 
+    val numberOfX = boardValue(0)
+    val numberOfO = boardValue(1)
+    var correctNumberOfMoves = false
+    if(numberOfX == numberOfO || numberOfX == numberOfO + 1 || numberOfX == numberOfO - 1) correctNumberOfMoves = true
+    return correctNumberOfMoves
   }
 
-  def winByCollom(): Boolean = {
-    var winByCollom = false
+  def checkForFullBoard(): Boolean = {
+    val boardValues = getBoardValues()
+    val numberOfX = boardValues(0)
+    val numberOfO = boardValues(1)
+    val fullBoard = 9
+  
+    var isFullBoard = false
+    if( numberOfX + numberOfO == fullBoard) isFullBoard = true
+
+    return isFullBoard 
+  }
+
+  def getBoardValues(): List[Int] = {
+  val rows = List(row1, row2, row3)
+
+    var numberOfX = 0
+    var numberOfO = 0
+
+    rows.foreach { row => {
+      numberOfX += row.toList.count(element => element == "X")
+      numberOfO += row.toList.count(element => element == "O")
+    }}
+
+    return List(numberOfX, numberOfO)
+  }
+
+  def checkForWin(): String = {
+    var winningMarking = ""
+    val rows = winByRow() 
+    val colloms = winByCollom()
+    val diagnals = winByDiagnal()
+
+    if (rows != "") winningMarking = rows
+    if (colloms != "") winningMarking = colloms
+    if (diagnals != "") winningMarking = diagnals
+    return winningMarking
+  }
+
+  def winByRow(): String = {
+    var winningMarking = ""
+    if( row1.toList.distinct.length == 1 && (row1(0) == "X" || row1(0) == "O")) winningMarking = row1(0)
+    if( row2.toList.distinct.length == 1 && (row2(0) == "X" || row2(0) == "O")) winningMarking = row2(0)
+    if( row3.toList.distinct.length == 1 && (row3(0) == "X" || row3(0) == "O")) winningMarking = row3(0)
+    return winningMarking
+  }
+
+  def winByCollom(): String = {
+    var winningMarking = ""
 
     var index = 0
     row1.toList.foreach { element => {
-      val value = row2(index) == element && 
+      val isWin = row2(index) == element && 
         row3(index) == element
-        if (value) winByCollom = value
+        if (isWin && (row1(0) == "X" || row1(0) == "O")) winningMarking = element
         index += 1
       }}
-    return winByCollom
+    return winningMarking
   }
 
-  def winByDiagnal(): Boolean = {
+  def winByDiagnal(): String = {
     val centerValue = row2(1)
-    val winByDiagnal = (row1(0) == centerValue && row3(2) == centerValue) || 
+    var winningMarking = ""
+    val isWin = (row1(0) == centerValue && row3(2) == centerValue) || 
       (row1(2) == centerValue && row3(0) == centerValue) 
-    return winByDiagnal
+    if(isWin && (row1(0) == "X" || row1(0) == "O")) winningMarking = centerValue
+    return winningMarking 
   }
 
   def printBoard() = {
