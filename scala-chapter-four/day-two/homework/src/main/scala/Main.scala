9926a6498872606f92dc51923158fa6ce7ab9509
Joshuah Boardman <joshuahboardman@gmail.com>
2023-03-19 22:40:57 -0500

    Update: finished second homework assignment day-two

--- scala-chapter-four/day-two/homework/src/main/scala/Main.scala
+++ scala-chapter-four/day-two/homework/src/main/scala/Main.scala
@@ -1,9 +1,21 @@
 @main def hello: Unit =  {
-  val theWords = List("yo", "hi", "swiga", "this actually works", "wow")
+  val theWords = List("yo", "hi", "swiga", "this actually works", "wow", "daRn", "Shoot")
   val theNumber = assignmentOne(theWords)
   println(theNumber)
+  val secondAssignment = assignmentTwo(theWords)
+  println(secondAssignment)
 }
 
 def assignmentOne(words:List[String]): Int = {
   words.foldLeft(0)((sum, value) => sum + value.size)
+}
+
+def assignmentTwo(words:List[String]): List[String] = {
+  val filteredList = words.map(word => {
+    var filteredWord = word
+   if(word.toLowerCase == "darn") filteredWord = "Beans"
+   if(word.toLowerCase == "shoot") filteredWord = "Pucky"
+   filteredWord
+  })
+  filteredList
 }
